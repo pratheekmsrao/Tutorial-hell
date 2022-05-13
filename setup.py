16c2181df2ec3f0ac126075d7e9503d08ca4c0d2
Pratheekmsrao <pratheekmsdev@gmail.com>
2022-05-14 00:35:39 +0530

    setup.py created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Tutorial-hell',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Pratheek M S',
+    author_email='',
+    description=''
+)
